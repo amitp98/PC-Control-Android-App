12c7c4239e9ee2fd2b227806b717b9b0547676cd
Amit Pakhare <ampmail1998@gmail.com>
2018-10-02 18:36:55 +0530

    minor changes

--- JavaApp/src/RemoteDesktop/MouseKey.java
+++ JavaApp/src/RemoteDesktop/MouseKey.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package RemoteDesktop;
 
 import java.awt.AWTException;
@@ -343,8 +339,6 @@ public class MouseKey {
     public void Move(int x, int y) {
         robot.mouseMove(x, y);
     }
-    
-    
 }
 
 
